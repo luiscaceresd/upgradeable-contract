b0968e6b8c94ae4648d03b0c49d5754c6725bcd0
luiscaceresd <luiscaceresd97@gmail.com>
2024-04-28 16:36:10 -0300

    adding contracts

---
+++ src/BoxV2.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract BoxV1 {
+    uint256 internal number;
+
+    function setNumber(uint256 _number) external {
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+}
