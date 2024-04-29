b20b6cfd3bc2ca3b72363b09dc1b4c52c51ba1f4
luiscaceresd <luiscaceresd97@gmail.com>
2024-04-29 20:45:41 -0300

    adding initializer

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -3,9 +3,21 @@
 pragma solidity ^0.8.18;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable{
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
