771d84158dcd35148890620cd503fbe236535cf4
luiscaceresd <luiscaceresd97@gmail.com>
2024-05-01 22:14:16 -0300

    adding upgrade contract

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -3,18 +3,26 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BoxV1} from "../src/BoxV1.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BoxV2} from "../src/BoxV2.sol";
 
-contract DeployBox is Script {
+contract UpgradeBox is Script {
   function run() external returns (address) {
-    address proxy = deployBox();
+    address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+
+    vm.startBroadcast();
+    BoxV2 newBox = new BoxV2();
+    vm.stopBroadcast();
+    address proxy = upgradeBox(mostRecentlyDeployed, address(newBox));
     return proxy;
   }
 
-  function deployBox() public returns (address) {
-    BoxV1 box = new BoxV1();
-    ERC1967Proxy proxy = new ERC1967Proxy(address(box), "");
+  function upgradeBox(address proxyAddress, address newBox) public returns(address) {
+    vm.startBroadcast();
+    BoxV1 proxy = BoxV1(proxyAddress);
+    proxy.upgradeToAndCall(address(newBox), bytes(""));
+    vm.stopBroadcast();
     return address(proxy);
   }
 }
